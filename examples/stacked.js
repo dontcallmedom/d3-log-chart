333cc3d19ec244a6c3aa724d20c9e5c81915dc27
Dominique Hazael-Massieux <dom@w3.org>
2015-03-22 18:34:25 +0100

    avoid mixed content block

--- examples/stacked.js
+++ examples/stacked.js
@@ -2,7 +2,7 @@
 requirejs.config({
   paths: {
       ramda: "https://cdnjs.cloudflare.com/ajax/libs/ramda/0.11.0/ramda",
-      d3: "http://d3js.org/d3.v3"
+      d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"
   }
 });
 
